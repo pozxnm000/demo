9a43fbe7de4cefc5e62fd2d9c5afb934f564f9ae
DESKTOP-G92I9IE\user <hello@hello.com>
2018-12-19 22:14:08 +0800

    Java and Kotlin 變數

--- src/main/java/com/haohao/demo/Hello.java
+++ src/main/java/com/haohao/demo/Hello.java
@@ -2,8 +2,16 @@ package com.haohao.demo;
 
 public class Hello {
     public static void main(String[] args) {
-//        System.out.println("test");
-            Parson p = new Parson();
-            p.hello();
+        //        Syste m.out.println("test");
+
+        String name = "sonic";
+        int age = 21;
+        Float height = 165.2f;
+        Float weight = 62.2f;
+        boolean error = true;
+        char numchar = '1';
+
+        String n = new String("abc");
+        System.out.println(n);
     }
 }
--- src/main/kotlin/com/haoaho/demo/Hello.kt
+++ src/main/kotlin/com/haoaho/demo/Hello.kt
@@ -4,6 +4,20 @@ fun main(args: Array<String>) {
 //    println("tefe")
     val h = Human()
     h.hello()
+
+    var name: String;
+
+    var age = 21
+    var weight = 33.2f
+    var height = 32.1f
+    val max = 100
+    val min = 0
+    
+    name = "sonic"
+
+    println(name)
+
+
 }
 
 class Human {
